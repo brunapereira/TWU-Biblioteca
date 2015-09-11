6a519289d786247210131a7087cfaef4e2360520
Bruna <ppbruna2@gmail.com>
2015-09-11 18:24:16 -0300

    created constructor to all attributes

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -4,4 +4,16 @@ package com.twu.biblioteca;
  * Created by bpereira on 9/11/15.
  */
 public class Book {
+
+    private boolean availability;
+    private String name;
+    private String author;
+    private String year;
+
+    public Book(boolean availability, String name, String author, String year) {
+        this.availability = availability;
+        this.name = name;
+        this.author = author;
+        this.year = year;
+    }
 }
