6f49b87d3d2aae6ee9828cc81a991c60e8d29fc2
Bruna <ppbruna2@gmail.com>
2015-09-11 17:20:38 -0300

    added test file to biblioteca

---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,23 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bpereira on 9/11/15.
+ */
+public class BibliotecaAppTest {
+
+    @Before
+    public void setUp() throws Exception {
+        
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
