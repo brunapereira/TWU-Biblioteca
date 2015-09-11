e56f80b7c73c0b8f4a2aabaf80b9d4ed06d285c6
Bruna <ppbruna2@gmail.com>
2015-09-11 18:16:13 -0300

    created class Book and BookTest

---
+++ src/com/twu/biblioteca/Book.java
@@ -0,0 +1,7 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by bpereira on 9/11/15.
+ */
+public class Book {
+}
---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bpereira on 9/11/15.
+ */
+public class BookTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
