aa88dfd0c55c2264c4f7538b8f6d73b8e3fcc8b4
Bruna <ppbruna2@gmail.com>
2015-09-14 12:56:27 -0300

    added books example to Biblioteca Test

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -3,7 +3,7 @@ package com.twu.biblioteca;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 /**
  * Created by bpereira on 9/11/15.
@@ -14,7 +14,11 @@ public class BibliotecaAppTest {
 
     @Before
     public void setUp() throws Exception {
-        biblioteca = new BibliotecaApp();
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book(true, "Book 1", "Bruna", "1992"));
+        books.add(new Book(true, "Book 2", "Julia", "1999"));
+        books.add(new Book(true, "Book 2", "Fernanda", "2003"));
+        biblioteca = new BibliotecaApp(books);
     }
 
     @Test
