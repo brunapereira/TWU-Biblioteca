4bd94a430c2f54b7a37cc3932600f1b74dc4e7dd
Bruna <ppbruna2@gmail.com>
2015-09-18 14:53:34 -0300

    added functionalities to book. rent and give back

---
+++ src/com/twu/biblioteca/Movie.java
@@ -0,0 +1,72 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by bpereira on 9/18/15.
+ */
+public class Movie {
+    private boolean availability;
+    private String name;
+    private String year;
+    private String director;
+    private int rating;
+
+    public Movie(boolean availability, String name, String year, String director, int rating) {
+        this.availability = availability;
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setAvailability(boolean a) {
+        this.availability = a;
+    }
+
+    public String rent() {
+        String result;
+        if (this.isAvailability()) {
+            this.setAvailability(false);
+            result = "Thank you! Enjoy the movie.";
+        }
+        else
+            result = "Movie Already Rented!";
+
+        System.out.println(result);
+        return result;
+    }
+
+    public String giveBack(){
+        String result;
+        if (!this.isAvailability()) {
+            this.setAvailability(true);
+            result = "Thank you for returning the movie!";
+        }
+        else
+            result = "This movie is already here!";
+
+        System.out.println(result);
+        return result;
+    }
+
+
+}
---
+++ test/com/twu/biblioteca/MovieTest.java
@@ -0,0 +1,82 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bpereira on 9/18/15.
+ */
+public class MovieTest {
+
+    Movie movie;
+
+    @Before
+    public void setUp() throws Exception {
+        movie = new Movie(true, "Pokemon", "1999", "Bruna", 5);
+    }
+
+    @Test
+    public void availabilityIsTrue() throws Exception {
+        assertEquals(movie.isAvailability(), true);
+    }
+
+    @Test
+    public void nameIsPokemon() throws Exception {
+        assertEquals(movie.getName(), "Pokemon");
+    }
+
+    @Test
+    public void yearIs1999() throws Exception {
+        assertEquals(movie.getYear(), "1999");
+    }
+
+    @Test
+    public void directorIsBruna() throws Exception {
+        assertEquals(movie.getDirector(), "Bruna");
+    }
+
+    @Test
+    public void ratingIs5() throws Exception {
+        assertEquals(movie.getRating(), 5);
+    }
+
+    @Test
+    public void movieIsUnavailableWhenRented() throws Exception {
+        movie.rent();
+        assertEquals(movie.isAvailability(), false);
+    }
+
+    @Test
+    public void movieIsAvailableWhenReturned() throws Exception {
+        movie.setAvailability(false);
+        movie.giveBack();
+        assertEquals(movie.isAvailability(), true);
+    }
+
+    @Test
+    public void movieCantBeRentedIfAvailabilityIsFalse() throws Exception {
+        movie.setAvailability(false);
+        assertEquals("Movie Already Rented!", movie.rent());
+    }
+
+    @Test
+    public void movieCantBeReturnedIfAvailabilityIsTrue() throws Exception {
+        movie.setAvailability(true);
+        assertEquals("This movie is already here!", movie.giveBack());
+    }
+
+    @Test
+    public void movieRentedCorrectly() throws Exception {
+        movie.setAvailability(true);
+        assertEquals("Thank you! Enjoy the movie.", movie.rent());
+    }
+
+    @Test
+    public void movieReturnedCorrectly() throws Exception {
+        movie.setAvailability(false);
+        assertEquals("Thank you for returning the movie!", movie.giveBack());
+    }
+
+}
